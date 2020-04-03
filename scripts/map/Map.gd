67b6cf80ec4256f4b31d7d8c47cddc6cfc16b921
Di4ik3r <di4iker@gmail.com>
2020-04-03 11:52:53 +0300

    map gen scene ui implementation; camera floating

--- scripts/map/Map.gd
+++ scripts/map/Map.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 
-const CAMERA_HEIGHT = 50
+const CAMERA_HEIGHT = 12
 
 var mapGenerator: MapGenerator = MapGenerator.new()
 var mapVars: Resource = mapGenerator.MapVars
@@ -25,17 +25,19 @@ func _update() -> void:
 	var cube_size = mapVars.CUBE_SIZE
 	var a = mapVars.a_side
 	var b = mapVars.b_side
-	print("%d : %d" % [a, b])
 	
-	circle_radius = 120
+	var max_side: = max(a, b) as int
+	var min_side: = min(a, b) as int
+	circle_radius = max_side / 2 + min_side / 2
 	
-	circle_center = Vector3(a / 2, CAMERA_HEIGHT, b / 2)
+	circle_center = Vector3(a / 2, 10 / 2, b / 2)
 	CenterPos.translation = circle_center
 
 func _physics_process(delta: float) -> void:
-	current_angle += delta
+	current_angle += delta / 10
 	CameraFollowPos.translation = _circle_equation(current_angle)
 	CameraFollowPos.look_at(CenterPos.translation, Vector3.UP)
+	CenterPos.look_at(CameraFollowPos.translation, Vector3.UP)
 
 func _circle_equation(angle: float) -> Vector3:
 	var x = circle_center.x + circle_radius * cos(angle)
@@ -44,139 +46,18 @@ func _circle_equation(angle: float) -> Vector3:
 
 
 
-#export(Resource) var map_vars: Resource
-
-
-#export(float, 0, 100, 0.01) var block_height_multiplier: float = 3 setget _set_block_height_multiplier
-#export(int, 0, 40, 1) var noise_multiplier: int = 6 setget _set_noise_multiplier
-
-#export(float, 0, 1, 0.01) var noise_persistance = 0.25 setget _set_noise_persistance
-#export(int, 0, 30, 1) var noise_octaves = 2 setget _set_noise_octaves
-#export(float, 0, 10, 0.1) var noise_lactunarity = 2 setget _set_noise_lactunarity
-
-#export(float, -1, 5, 0.001) var height_addition: float = 2.8 setget _set_height_addition
-#export(float, -1, 1, 0.001) var sand_comparison = 0.2 setget _set_sand_comparison
-
-
-
-#export(float, 0, 2, 0.001) var mountain_comparison: float = 2 setget _set_mountain_comparison
-
-#export(Color) var color_grass_interpolate: Color = Color(0, 0.7, 0) setget _set_color_grass_interpolate
-#export(Color) var color_grass: Color = Color(0.1, 0.8, 0) setget _set_color_grass
-
-#export(Color) var color_grass_interpolate: Color = Color(0.2, 0.6, 0) setget _set_color_grass_interpolate
-#export(Color) var color_grass: Color = Color(0.5, 0.8, 0) setget _set_color_grass
-#export(Color) var color_sand_interpolate: Color = Color.black setget _set_color_sand_interpolate
-#export(Color) var color_sand: Color = Color(0.6, 0.4, 0) setget _set_color_sand
-#export(Color) var color_water_interpolate := Color(0, 0.5, 1) setget _set_color_water_interpolate
-#export(Color) var color_water := Color.blue setget _set_color_water
-#export(Color) var color_mountain_interpolate := Color.white setget _set_color_mountain_interpolate
-#export(Color) var color_mountain := Color(0.69, 0.69, 0.69) setget _set_color_mountain
-
-#export(float, -2, 0, 0.001) var water_height: float = -0.2 setget _set_water_height
-#export(float, -1, 1, 0.001) var water_comparison: float = -0.4 setget _set_water_comparison
-
-#var forest_blocks: = Array()
-
-
-
-#onready var Terrain := $Terrain as MultiMeshInstance
-#onready var Trees := $"Terrain/Trees" as MultiMeshInstance
-
-
-#func _init() -> void:
-#	_terrain_noise_configuration()
-#	_trees_noise_configuration()
-#	if Engine.editor_hint:
-#		Terrain = get_child(0) as MultiMeshInstance
-#		if Terrain:
-#			Trees = Terrain.get_child(0) as MultiMeshInstance
-
-#func _ready() -> void:
-#	_init()
-#	_update_Terrain()
-
-#func _process(delta: float) -> void:
-#	if not Engine.editor_hint:
-#		SceneLight.rotate_x(delta)
-
-
-
-
-
-
-
-# ########################################################################################### SETTERS
-
-
-
-#func _set_noise_multiplier(value: int) -> void:
-#	noise_multiplier = value
-#	_update_Terrain()
-#func _set_height_addition(value: float) -> void:
-#	height_addition = value
-#	_update_Terrain()
-#func _set_water_comparison(value: float) -> void:
-#	water_comparison = value
-#	_update_Terrain()
-#func _set_sand_comparison(value: float) -> void:
-#	sand_comparison = value
-#	_update_Terrain()
-#func _set_color_grass(value: Color) -> void:
-#	color_grass = value
-#	_update_Terrain()
-#func _set_color_sand(value: Color) -> void:
-#	color_sand = value
-#	_update_Terrain()
-#func _set_color_water(value: Color) -> void:
-#	color_water = value
-#	_update_Terrain()
-#func _set_water_height(value: float) -> void:
-#	water_height = value
-#	_update_Terrain()
-#func _set_noise_persistance(value: float) -> void:
-#	noise_persistance = value
-#	_terrain_noise_configuration()
-#	_update_Terrain()
-#func _set_noise_octaves(value: int) -> void:
-#	noise_octaves = value
-#	_terrain_noise_configuration()
-#	_update_Terrain()
-#func _set_color_grass_interpolate(value: Color) -> void:
-#	color_grass_interpolate = value
-#	_update_Terrain()
-#func _set_color_sand_interpolate(value: Color) -> void:
-#	color_sand_interpolate = value
-#	_update_Terrain()
-#func _set_noise_lactunarity(value: float) -> void:
-#	noise_lactunarity = value
-#	_update_Terrain()
-#func _set_color_water_interpolate(value: Color) -> void:
-#	color_water_interpolate = value
-#	_update_Terrain()
-#func _set_block_height_multiplier(value: float) -> void:
-#	block_height_multiplier = value
-#	_update_Terrain()
-#func _set_mountain_comparison(value: float) -> void:
-#	mountain_comparison = value
-#	_update_Terrain()
-#func _set_color_mountain(value: Color) -> void:
-#	color_mountain = value
-#	_update_Terrain()
-#func _set_color_mountain_interpolate(value: Color) -> void:
-#	color_mountain_interpolate = value
-#	_update_Terrain()
-
 
 func _on_MapGeneratingUI_length_changed(value: int) -> void:
 	mapVars.a_side = value
-	mapGenerator.update_Terrain()
+	mapGenerator.update_Terrain(true)
+	_update()
 func _on_MapGeneratingUI_width_changed(value: int) -> void:
 	mapVars.b_side = value
-	mapGenerator.update_Terrain()
+	mapGenerator.update_Terrain(true)
+	_update()
 func _on_MapGeneratingUI_seed_changed(value: float) -> void:
 	mapVars.noise_seed = value
-	mapGenerator.update_Terrain()
+	mapGenerator.update_Terrain(true)
 func _on_MapGeneratingUI_forest_comparison_changed(value) -> void:
 	mapVars.forest_comparison = value
 	mapGenerator.update_Terrain()
--- scripts/map/MapGenerator.gd
+++ scripts/map/MapGenerator.gd
@@ -12,37 +12,33 @@ export(Resource) var MapVars = preload("res://resources/map/MapVars.tres")
 func set_mminstance(_Terrain: MultiMeshInstance, _Trees: MultiMeshInstance) -> void:
 	Terrain = _Terrain
 	Trees = _Trees
-	print(Terrain)
 	
 func change_parameter(parameter: String, value) -> void:
 	MapVars[parameter] = value
 
-func _generate() -> void:
+var min_height
+var max_height
+func _generate(isTragic: bool) -> void:
 	var forest_blocks = Array()
-	var min_height: float = 999.0
-	var max_height: float = -999.0
-	for a in range(0, MapVars.a_side):
-		for b in range(0, MapVars.b_side):
-			var noise: float = MapVars.noise.get_noise_2d(a, b) * MapVars.NOISE_MULTIPLIER
-			var height: float = MapVars.BLOCK_HEIGHT_MULTIPLIER * MapVars.noise.get_noise_2d(a, b)
-
-			if height > max_height:
-				max_height = height
-
-			if height < min_height:
-				min_height = height
+	if isTragic or not min_height or not max_height:
+		min_height = 999
+		max_height = -999
+		for a in range(0, MapVars.a_side):
+			for b in range(0, MapVars.b_side):
+				var noise: float = MapVars.noise.get_noise_2d(a, b) * MapVars.NOISE_MULTIPLIER
+				var height: float = MapVars.BLOCK_HEIGHT_MULTIPLIER * MapVars.noise.get_noise_2d(a, b)
+	
+				if height > max_height:
+					max_height = height
+	
+				if height < min_height:
+					min_height = height
 #
-	var full_height: float
-	if min_height < 0:
-		full_height = max_height + abs(min_height)
-	else:
-		full_height = max_height
-
 	var instance: int = 0
 	for a in range(0, MapVars.a_side):
 		for b in range(0, MapVars.b_side):
-			var noise: float = MapVars.noise.get_noise_2d(a, b) * MapVars.NOISE_MULTIPLIER
-			var height_compare: float = noise + MapVars.HEIGHT_ADDITION
+			var noise = MapVars.noise.get_noise_2d(a, b) * MapVars.NOISE_MULTIPLIER
+			var height_compare = noise + MapVars.HEIGHT_ADDITION
 			var height = MapVars.BLOCK_HEIGHT_MULTIPLIER * MapVars.noise.get_noise_2d(a, b)
 
 			if height_compare < MapVars.WATER_COMPARISON:
@@ -78,71 +74,55 @@ func _generate() -> void:
 
 # ########################################################################################### BLOCK GEN
 func _generate_water(noise: float, instance: int) -> float:
-	var color: Color
-	color = Color(MapVars.COLOR_WATER.to_html())
-	color = color.linear_interpolate(MapVars.COLOR_WATER_INTERPOLATION,
+	var color: = Color(MapVars.COLOR_WATER.to_html()).linear_interpolate(MapVars.COLOR_WATER_INTERPOLATION,
 		Tools.noise_float_lerp(noise) * noise + 1)
 	Terrain.multimesh.set_instance_color(instance, color)
-	var height = MapVars.WATER_HEIGHT - MapVars.BLOCK_HEIGHT_MULTIPLIER / 1.89
-	return height
+	return MapVars.WATER_HEIGHT - MapVars.BLOCK_HEIGHT_MULTIPLIER / 1.89
 
 func _generate_sand(noise: float, instance: int) -> void:
-	var color: Color
-	color = Color(MapVars.COLOR_SAND.to_html())
-	color = color.linear_interpolate(MapVars.COLOR_SAND_INTERPOLATE,
+	var color: = Color(MapVars.COLOR_SAND.to_html()).linear_interpolate(MapVars.COLOR_SAND_INTERPOLATE,
 		Tools.noise_float_lerp(noise) + 0.2)
 	Terrain.multimesh.set_instance_color(instance, color)
 
 func _generate_mountain(min_height: float, height: float, noise:float, instance: int) -> float:
-	var color: Color
+	var color: = Color(MapVars.COLOR_MOUNTAIN.to_html()).linear_interpolate(MapVars.COLOR_MOUNTAIN_INTERPOLATION,
+		Tools.noise_float_lerp(noise) * 5 - 10)
 	height *= noise * MapVars.MOUNTAIN_COMPARISON / 1.5
 	height -= MapVars.BLOCK_HEIGHT_MULTIPLIER / 2
-	color = Color(MapVars.COLOR_MOUNTAIN.to_html())
-	color = color.linear_interpolate(MapVars.COLOR_MOUNTAIN_INTERPOLATION,
-		Tools.noise_float_lerp(noise) * 5 - 10)
+	
 	Terrain.multimesh.set_instance_color(instance, color)
 
 	return height
 
 func _generate_tree(a: int, b: int, min_height: float, height: float, noise: float, instance: int) -> Vector3:
-	var color: Color
-	color = Color(MapVars.COLOR_GRASS.to_html())
-	color = color.linear_interpolate(MapVars.COLOR_GRASS_INTERPOLATION,
+	var color: = Color(MapVars.COLOR_GRASS.to_html()).linear_interpolate(MapVars.COLOR_GRASS_INTERPOLATION,
 		Tools.noise_float_lerp(noise))
+	
 	Terrain.multimesh.set_instance_color(instance, color)
 
-	var calc: float
-	if MapVars.BLOCK_HEIGHT_MULTIPLIER == 0:
-		calc = 1
-	else:
-		calc = (abs(min_height) + height) * MapVars.BLOCK_HEIGHT_MULTIPLIER / (MapVars.BLOCK_HEIGHT_MULTIPLIER / 2) + 1
+#	var calc: float = (abs(min_height) + height) * MapVars.BLOCK_HEIGHT_MULTIPLIER / (MapVars.BLOCK_HEIGHT_MULTIPLIER / 2) + 1
+	var calc: float = (abs(min_height) + height) * 2 + 1
 	var x_offset = rand_range(-0.3, 0.3)
 	var z_offset = rand_range(-0.3, 0.3)
-	var pos = Vector3(a, calc + 1, b)
-#	var pos = Vector3(a + x_offset, calc + 1, b + z_offset)
+	var pos = Vector3(a, calc + 0.5, b)
+#	var pos = Vector3(a + x_offset, calc + 0.5, b + z_offset)
 
 	return pos
 
 func _generate_grass(noise, instance) -> void:
-	var color: Color
-	color = Color(MapVars.COLOR_GRASS.to_html())
-	color = color.linear_interpolate(MapVars.COLOR_GRASS_INTERPOLATION,
+	var color: = Color(MapVars.COLOR_GRASS.to_html()).linear_interpolate(MapVars.COLOR_GRASS_INTERPOLATION,
 		Tools.noise_float_lerp(noise) )
 	Terrain.multimesh.set_instance_color(instance, color)
 
 func _place_block(a: int, b: int, min_height:float, height: float,  noise: float, instance: int) -> void:
 	var pos = Vector3(a, (height + abs(min_height) + MapVars.CUBE_SIZE / 2), b)
 	var basis = Basis()
-	var calc: float
-	if MapVars.BLOCK_HEIGHT_MULTIPLIER == 0:
-		calc = 1
-	else:
-		calc = (abs(min_height) + height) * MapVars.BLOCK_HEIGHT_MULTIPLIER / (MapVars.BLOCK_HEIGHT_MULTIPLIER / 2) + 1
+	var calc: float = (abs(min_height) + height) * 2 + 1
 	basis = basis.scaled(Vector3(1, calc, 1))
 	var transform = Transform(basis, pos)
 	Terrain.multimesh.set_instance_transform(instance, transform)
 
-func update_Terrain() -> void:
+func update_Terrain(isTragic: bool = false) -> void:
 	seed(MapVars.noise_seed)
 	Terrain.multimesh.instance_count = MapVars.a_side * MapVars.b_side
-	_generate()
+	_generate(isTragic)
